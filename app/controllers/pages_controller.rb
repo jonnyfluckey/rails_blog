194efd94687399f1a3d990b31265ff7313839964
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-02-25 20:58:11 -0700

    End of Lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,37 @@ class PagesController < ApplicationController
     # Page.create (stores it in the Database)
     @page = Page.new # Stores it in Memory
   end
+
+  def create
+    @page = Page.new(pages_params)
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id].destroy)
+    redirect_to pages_path
+  end
+
+  private
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)    
+    end
+    #{ page: {title: 'foo', author: 'body', body: 'this'}}
 end
